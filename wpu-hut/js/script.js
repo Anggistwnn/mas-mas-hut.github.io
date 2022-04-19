c5af21973ebf1cc1817d3da71637dca822cc68b9
Anggistwnn <anggistwnn@gmail.com>
2022-04-19 17:37:22 +0700

    update rest api menggunakan js

---
+++ wpu-hut/js/script.js
@@ -0,0 +1,7 @@
+$.getJSON('data/pizza.json', function (data) {
+    let menu = data.menu;
+    $.each(menu, function (i, data) {
+        $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + data.gambar + '" class=" card-img-top"alt="american favorite"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text"> ' + data.deskripsi + ' </p><h5>Rp ' + data.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang!</a></div></div></div>')
+    });
+
+});
